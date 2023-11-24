03b8ffab5d7b776e193286d4c53de1e192dfd973
somadina6 <soma@Somadinas-MacBook-Pro.local>
2023-11-24 00:21:48 -0400

    testing static directory

--- deka_naturals_project/settings.py
+++ deka_naturals_project/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    
-]
+# STATICFILES_DIRS = []
      
 
 # Default primary key field type
